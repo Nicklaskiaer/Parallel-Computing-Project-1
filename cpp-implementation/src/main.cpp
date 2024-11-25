ccafd0d8cd4d9d2d18ff076b0548747d4d1298e8
nicklas.kiaer <nicklas.kiaer@studenti.unitn.it>
2024-11-25 13:42:02 +0100

    added random matrix initialization option

--- cpp-implementation/src/main.cpp
+++ cpp-implementation/src/main.cpp
@@ -1,20 +1,34 @@
 #include "main.h"
 
 int main() {
-    int n;
+    int n, r;
     int print = 0;
 
     std::cout << "Enter the size of the matrix: ";
     std::cin >> n;
-    std::cout << "Do you want to print the matrices? (1=yes/0=no): ";
+    std::cout << "Do you want to print the matrices? (1/0): ";
     std::cin >> print;
+    std::cout << "Should the matrix be random? (1/0): ";
+    std::cin >> r;
+    std::cout << std::endl;
 
     // Creating the matrix
     int **matrix = new int *[n];
-    for (int i = 0; i < n; i++) {
-        matrix[i] = new int[n];
-        for (int j = 0; j < n; j++) {
-            matrix[i][j] = i + j;
+    if (r) {
+        std::srand(std::time(0));
+        for (int i = 0; i < n; i++) {
+            matrix[i] = new int[n];
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] =
+                    std::rand() % 100;  // Random values between 0 and 99
+            }
+        }
+    } else {
+        for (int i = 0; i < n; i++) {
+            matrix[i] = new int[n];
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = i + j;
+            }
         }
     }
 
@@ -22,12 +36,21 @@ int main() {
     symetryResult resSymImp = timeCheckSymFunction(checkSymImp, matrix, n);
     symetryResult resSymOMP = timeCheckSymFunction(checkSymOMP, matrix, n);
 
-    std::cout << "Time taken by checkSymSeq: " << resSymSeq.duration
-              << " seconds" << std::endl;
-    std::cout << "Time taken by checkSymImp: " << resSymImp.duration
-              << " seconds" << std::endl;
-    std::cout << "Time taken by checkSymOMP: " << resSymOMP.duration
-              << " seconds" << std::endl;
+    std::cout << "CheckSym Sequential:\n"
+              << "Symetric: " << (resSymSeq.isSymetric ? "true" : "false")
+              << "\n"
+              << "Time taken: " << resSymSeq.duration << " seconds\n"
+              << std::endl;
+    std::cout << "CheckSym Implicit:\n"
+              << "Symetric: " << (resSymImp.isSymetric ? "true" : "false")
+              << "\n"
+              << "Time taken: " << resSymImp.duration << " seconds\n"
+              << std::endl;
+    std::cout << "CheckSym Explicit:\n"
+              << "Symetric: " << (resSymOMP.isSymetric ? "true" : "false")
+              << "\n"
+              << "Time taken: " << resSymOMP.duration << " seconds\n"
+              << std::endl;
 
     transposeResult resTransSeq = timeTransposeFunction(transpose, matrix, n);
     transposeResult resTransImp =
@@ -48,18 +71,6 @@ int main() {
         printMatrix(resTransImp.new_matrix, n, "Transposed Implicit");
         printMatrix(resTransOMP.new_matrix, n, "Transposed Explicit");
     }
-
-    // Clean up
-    for (int i = 0; i < n; i++) {
-        delete[] matrix[i];
-        delete[] resTransSeq.new_matrix[i];
-        delete[] resTransImp.new_matrix[i];
-        delete[] resTransOMP.new_matrix[i];
-    }
-    delete[] matrix;
-    delete[] resTransSeq.new_matrix;
-    delete[] resTransImp.new_matrix;
-    delete[] resTransOMP.new_matrix;
 
     return 0;
 }
